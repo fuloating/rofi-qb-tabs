6609651f432ce698eccd9a31f9c67c335ae63753
fuloating <isfloating@yahoo.com>
2017-04-01 19:28:56 +0300

    Update tabs.sh

--- tabs.sh
+++ tabs.sh
@@ -6,7 +6,7 @@ ARG=$@
 if [ -n "${ARG}" ]
 then
     SEL_URL=$( echo ${ARG} | perl -ne 'if (/^(.+?)\s+(https?:\/\/\S+)\s*(.*)$/) {print "$2\n"}')
-    coproc ( my-qutebrowser "$SEL_URL" > /dev/null  2>&1 )
+    coproc ( xdg-open "$SEL_URL" > /dev/null  2>&1 )
     exec 1>&-
     grep -v "$ARG" "$TABFILE" > "temp.txt"
     cp "temp.txt" "$TABFILE"
